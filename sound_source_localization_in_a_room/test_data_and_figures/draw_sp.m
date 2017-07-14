a5e60c2e1b7ac67f7ed64f119f388dae5e54c164
Helena Peic Tukuljac <hela_su@yahoo.com>
2017-07-14 11:13:05 +0200

    Add files via upload

--- sound_source_localization_in_a_room/test_data_and_figures/draw_sp.m
+++ sound_source_localization_in_a_room/test_data_and_figures/draw_sp.m
@@ -1,22 +1,5 @@
 close all
 load data_sp.mat
-% sp_vector = 1:15;
-% results = zeros(length(sp_vector),1);   % reconstruction error
-% results_i = [0 28 42 75 90 110 102 132 119 139 142 122 117 143 141]; % iteration number
-% results_t = [0 3.75 4 5.6 6.2 6.7 5.4 7 5.8 6 5.9 5.3 5 5.7 5.6]; % iteration number
-% results_f = [100 0 0 4 6 16 27 32 37 40 47 45 55 62 58]; % failure counter
-
-figure('units','normalized','outerposition',[0 0 1 1])
-set(gcf,'color','w');
-plot(sp_vector, results, 'Linewidth', 3)
-xlabel('Subsampling level')
-ylabel('Reconstruction error')
-xlim([1 inf])
-ylim([0 inf])
-title('Average reconsruction error for different source postion subsampling')
-set(gca,'fontsize', 18)
-%saveas(gcf,'subsampling_over_sound_source_convergence.png')
-%close all
 
 figure('units','normalized','outerposition',[0 0 1 1])
 set(gcf,'color','w');
@@ -27,20 +10,6 @@ xlim([1 inf])
 ylim([0 inf])
 title('Average number of iterations')
 set(gca,'fontsize', 24)
-%saveas(gcf,'subsampling_over_sound_source_iterations.png')
-%close all
-
-figure('units','normalized','outerposition',[0 0 1 1])
-set(gcf,'color','w');
-plot(sp_vector, results_f, 'Linewidth', 3)
-xlabel('Subsampling level')
-ylabel('Number of failures')
-xlim([1 inf])
-ylim([0 inf])
-title('Number of failures')
-set(gca,'fontsize', 24)
-%saveas(gcf,'subsampling_over_sound_source_failures.png')
-%close all
 
 figure('units','normalized','outerposition',[0 0 1 1])
 set(gcf,'color','w');
@@ -51,5 +20,13 @@ xlim([1 inf])
 ylim([0 inf])
 title('Average reconstruction time')
 set(gca,'fontsize', 24)
-%saveas(gcf,'subsampling_over_sound_source_times.png')
-%close all
+
+figure('units','normalized','outerposition',[0 0 1 1])
+set(gcf,'color','w');
+plot(sp_vector, results_f, 'Linewidth', 3)
+xlabel('Subsampling level')
+ylabel('Number of failures')
+xlim([1 inf])
+ylim([0 inf])
+title('Number of failures')
+set(gca,'fontsize', 24)
